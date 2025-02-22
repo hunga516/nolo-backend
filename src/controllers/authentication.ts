36d742c55c2d6503eadd999f2d7dd8ff67e85909
Ngoc Loc <hunga5160@gmail.com>
2025-02-22 23:40:55 +0700

    revice

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import { getUserByEmail, getUserByUsername } from '../db/users'
+import { createUser, getUserByEmail, getUserByUsername } from '../db/users'
+import { authentication, random } from '../helpers'
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
@@ -17,6 +18,18 @@ export const register = async (req: express.Request, res: express.Response) => {
         if (existingUsername) {
             return res.sendStatus(400)
         }
+
+        const salt = random()
+        const user = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authentication(salt, password),
+            },
+        })
+
+        return res.status(200).json(user).end()
     } catch (e) {
         console.log(e)
         return res.sendStatus(400)
--- src/db/users.ts
+++ src/db/users.ts
@@ -19,7 +19,7 @@ export const getUserByEmail = (email: string) => UserModel.findOne({ email })
 export const getUserBySessionToken = (sessionToken: string) =>
     UserModel.findOne({ 'authentication.sessionToken': sessionToken })
 export const getUserById = (id: string) => UserModel.findById(id)
-export const createUser = (id: string, user: Record<string, any>) =>
+export const createUser = (user: Record<string, any>) =>
     new UserModel(user).save().then((user) => user.toObject())
 export const updateUser = (id: string, newUser: Record<string, any>) =>
     UserModel.findByIdAndUpdate(id, newUser, { new: true })
