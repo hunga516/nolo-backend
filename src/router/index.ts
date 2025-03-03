f6b28282d643f4d1d58978603f993dcdaeac5eef
Ngoc Loc <hunga5160@gmail.com>
2025-03-03 11:42:44 +0700

    basic project with login and register function

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import authentication from './authentication'
+import { register } from '../controllers/authentication'
 
 const router = express.Router()
 
 export default (): express.Router => {
     authentication(router)
+    router.post('/auth/register-test', register)
 
     return router
 }
