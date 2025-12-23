f1ef0219d2083db455912a914935aecbd267a6f5
hunga5160 <hunga5160@gmail.com>
2025-12-24 04:35:20 +0700

    save

--- dist/router/items.route.js
+++ dist/router/items.route.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = itemsRouter;
 const multer_1 = __importDefault(require("multer"));
 const items_controller_1 = require("../controllers/items.controller");
+const inventory_1 = require("../db/inventory");
+const user_1 = require("../db/user");
+const item_1 = require("../db/item");
 const upload = (0, multer_1.default)({ dest: 'uploads/' });
 function itemsRouter(router) {
     router.post('/items', upload.single('image'), items_controller_1.createItemController);
@@ -27,38 +30,38 @@ function itemsRouter(router) {
             //     accumulated: 0,
             //     id: 12741070
             // }
-            console.log("webhook goc sdasdad", req.body);
+            console.log("webhook goc", req.body);
             const data = req.body.content;
-            // const newData = data.split(" ")
             const newData = data.split("-").pop();
-            // const userId = Number(newData)
-            const itemName = newData[1];
-            // const existingUser = await readUserByUserId(8)
-            // const existingItem = await readItemByName("k co item")
-            // if (!existingItem) {
-            //     const existingUser = await readUserByUserId(8)
-            //     existingUser.coins += req.body.transferAmount
-            //     await existingUser.save()
-            //     return res.json({
-            //         message: "So du tai khoan da duoc cap nhat",
-            //     });
-            // }
-            // try {
-            //     const existingInventory = await readInventoryByUserIdAndItemId(existingUser._id, existingItem._id)
-            //     if (existingInventory) {
-            //         existingInventory.quantity += 1
-            //         await existingInventory.save()
-            //         return res.json(existingInventory)
-            //     }
-            //     const newInventory = await createInventory({ userId: existingUser._id, itemId: existingItem._id })
-            //     return res.json({
-            //         message: "Vat pham da duoc them thanh cong",
-            //         newInventory
-            //     })
-            // } catch (error) {
-            //     console.log();
-            //     return res.status(400)
-            // }
+            const userId = Number(newData);
+            // const itemName = newData[1]
+            const existingUser = await (0, user_1.readUserByUserId)(userId);
+            const existingItem = await (0, item_1.readItemByName)("null");
+            if (!existingItem) {
+                const existingUser = await (0, user_1.readUserByUserId)(userId);
+                existingUser.coins += req.body.transferAmount;
+                await existingUser.save();
+                return res.json({
+                    message: "So du tai khoan da duoc cap nhat",
+                });
+            }
+            try {
+                const existingInventory = await (0, inventory_1.readInventoryByUserIdAndItemId)(existingUser._id, existingItem._id);
+                if (existingInventory) {
+                    existingInventory.quantity += 1;
+                    await existingInventory.save();
+                    return res.json(existingInventory);
+                }
+                const newInventory = await (0, inventory_1.createInventory)({ userId: existingUser._id, itemId: existingItem._id });
+                return res.json({
+                    message: "Vat pham da duoc them thanh cong",
+                    newInventory
+                });
+            }
+            catch (error) {
+                console.log();
+                return res.status(400);
+            }
         }
         catch (error) {
             console.log(error);
